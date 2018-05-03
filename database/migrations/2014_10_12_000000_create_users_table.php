b94b1bf8067d3754c362f6df18300e895d7e35db
alexluong <aluong@crimson.ua.edu>
2018-05-03 04:33:23 -0500

    Added relational stuff

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role')->default('hacker');
-            $table->integer('app_id');
             $table->rememberToken();
             $table->timestamps();
         });
